de7d9fddc09a7b5f3ea17de195e3f8647bc6c2e9
Ronin <yuhaochen37@outlook.com>
2023-07-28 15:50:49 +1000

    added auth-features

--- src/pages/auth/forgot-pwd/ForgotPwd.js
+++ src/pages/auth/forgot-pwd/ForgotPwd.js
@@ -5,8 +5,34 @@ import { FaArrowLeft } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import backGroundImg from '../../../assets/images/background.jpg';
 import '../forgot-pwd/ForgotPwd.scss';
+import { authService } from '../../../services/api/auth/auth.service';
+import { useState } from 'react';
 
 const ForgotPwd = () => {
+  const [email, setEmail] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [showAlert, setShowAlert] = useState(false);
+  const [alertType, setAlertType] = useState('');
+  const [responseMessage, setResponseMessage] = useState('');
+
+  const ForgotPwd = async (event) => {
+    setLoading(true);
+    event.preventDefault();
+    try {
+      const response = await authService.forgotPassword(email);
+      setLoading(false);
+      setEmail('');
+      setShowAlert(false);
+      setAlertType('alert-success');
+      setResponseMessage(response?.data?.message);
+    } catch (error) {
+      setAlertType('alert-error');
+      setLoading(false);
+      setShowAlert(true);
+      setResponseMessage(error?.response?.data?.message);
+    }
+  };
+
   return (
     <div
       className="container-wrapper"
@@ -14,7 +40,10 @@ const ForgotPwd = () => {
     >
       <div className="environment">DEV</div>
       <div className="container-wrapper-auth">
-        <div className="tabs forgot-password-tabs">
+        <div
+          className="tabs forgot-password-tabs"
+          style={{ height: `${responseMessage ? '300px' : ''}` }}
+        >
           <div className="tabs-auth">
             <ul className="tab-group">
               <li className="tab">
@@ -24,25 +53,34 @@ const ForgotPwd = () => {
 
             <div className="tab-item">
               <div className="auth-inner">
-                {/* <div className="alerts" role="alert">
-              Error message
-            </div> */}
-                <form className="auth-form">
+                {responseMessage && (
+                  <div className={`alerts ${alertType}`} role="alert">
+                    {responseMessage}
+                  </div>
+                )}
+                <form className="auth-form" onSubmit={ForgotPwd}>
                   <div className="form-input-container">
                     <Input
-                      id="password"
-                      name="password"
+                      id="email"
+                      name="email"
                       type="text"
-                      value="pwd"
-                      labelText="Password"
-                      placeholder="Enter Password"
-                      handleChange={() => {}}
+                      value={email}
+                      labelText="Email"
+                      placeholder="Enter Email"
+                      style={{
+                        border: `${showAlert ? '1px solid #fa9b8a' : ''}`,
+                      }}
+                      handleChange={(e) => setEmail(e.target.value)}
                     />
                   </div>
                   <Button
-                    label={'FORGOT PASSWORD'}
+                    label={`${
+                      loading
+                        ? 'FORGOT PASSWORD IN PROGRESS...'
+                        : 'FORGOT PASSWORD'
+                    }`}
                     className="auth-button button"
-                    disabled={true}
+                    disabled={!email}
                   />
                   <Link to={'/'}>
                     <span className="forgot-password">
